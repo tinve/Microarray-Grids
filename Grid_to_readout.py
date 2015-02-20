7802975b889272011fca3f76cbb19d2c288f8fd6
Galina <malov-galina@yandex.ru>
2015-02-20 12:54:45 -0800

    grid files

--- Grid_to_readout.py
+++ Grid_to_readout.py
@@ -15,13 +15,14 @@ margin = 8
 # directory with grid files
 directory = "Grid"
 
+
+def grid_readouts(path, x0 = 0, y0 = 0, pix = 10, margin = 8):
+
 # loads grid file from the path, generates and returns two arrays:
 # readout_fast with physical fast coordinates
 # readout_slow with physical slow coordinates
 # each array has prepended number of readout points
 # referencing is in the fast direction
-
-def grid_readouts(path, x0=0, y0=0, pix=10, margin=8):
 
     grid = numpy.loadtxt(path, skiprows=1)  # txt file to list, skip header
     grid = numpy.array(grid).T.tolist()  # transpose to list of two lists (fast and slow)
@@ -41,29 +42,33 @@ def grid_readouts(path, x0=0, y0=0, pix=10, margin=8):
     readout_fast = to_readout(readout_fast, margin, pix)  # adds reference points to fast coordinates
 
     # adds "header" - number of readout points in coordinate sequence
-    readout_fast.insert(0, 2*columns + 1)
-    readout_slow.insert(0, rows)
+    readout_fast.insert(0, int(2*columns + 1))
+    readout_slow.insert(0, int(rows))
 
     readout_fast = rows * readout_fast
 
     return [readout_fast, readout_slow]
 
+
+def to_physical(grid, x0 = 0, y0 = 0, pix = 1):
+
 # takes list of fast and slow coordinates lists and converts into physical coordinates:
 # scales each pixel coordinate by pixel size (in microns) and adds x or
 
-def to_physical(grid, x0=0, y0=0, pix=1):
-    grid[0] = [pix*x+x0 for x in grid[0]]
-    grid[1] = [pix*y+y0 for y in grid[1]]
+    grid[0] = [pix*x + x0 for x in grid[0]]
+    grid[1] = [pix*y + y0 for y in grid[1]]
     return grid
 
 
+def to_readout(x, margin = 8, pix = 1):
+
 # takes a list of coordinates and inserts intermediate points between each two elements.
 # on edges adds reference points a margin distance away (in pixels)
-def to_readout(x, margin = 8, pix = 1):
+
     result = [x[0] - margin*pix]
-    for i in range(len(x)-1):
+    for i in range(len(x) - 1):
         result.append(x[i])
-        result.append((x[i]+x[i+1])/2)
+        result.append((x[i] + x[i+1])/2)
     result.append(x[-1])
     result.append(x[-1] + margin*pix)
     return result
@@ -72,13 +77,31 @@ readout_fast = []
 readout_slow = []
 
 files = os.listdir(directory)
+files.sort()
 
 # reads each grid file from the directory, generates readout fast and slow sequences and appends them together
-for f in files:
-#    print f
-    readouts = grid_readouts(directory + '/' + f, x0, y0, pix, margin)
-    readout_fast.append(readouts[0])
-    readout_slow.append(readouts[1])
+for f_name in files:
+    readouts = grid_readouts(os.path.join(directory, f_name), x0, y0, pix, margin)
+    readout_fast = readout_fast + readouts[0]
+    readout_slow = readout_slow + readouts[1]
 
-numpy.savetxt("readout_fast.txt", readout_fast, fmt = "%f", delimiter = "\n")
-numpy.savetxt("readout_slow.txt", readout_slow, fmt = "%f", delimiter = "\n")
+ff = open("readout_fast.txt", "w")
+fs = open("readout_slow.txt", "w")
+
+for coord in readout_fast:
+    if isinstance(coord, int):
+        ff.write("%i\n" % coord)
+    else:
+        ff.write("%f\n" % coord)
+
+for coord in readout_slow:
+    if isinstance(coord, int):
+        fs.write("%i\n" % coord)
+    else:
+        fs.write("%f\n" % coord)
+
+#numpy.savetxt("readout_fast.txt", readout_fast, fmt = "%f", delimiter = "\n")
+#numpy.savetxt("readout_slow.txt", readout_slow, fmt = "%f", delimiter = "\n")
+
+ff.close()
+fs.close()
